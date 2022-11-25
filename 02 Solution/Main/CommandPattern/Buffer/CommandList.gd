fec82950f4c537627965e6fc58ad72811442c811
Arpit Srivastava <skyvastern@gmail.com>
2022-11-25 19:53:23 +0530

    Replace word _queue with _array for undo/redo arrays

--- 02 Solution/Main/CommandPattern/Buffer/CommandList.gd
+++ 02 Solution/Main/CommandPattern/Buffer/CommandList.gd
@@ -1,47 +1,47 @@
 extends Node
 
-var _undo_queue: Array = []
-var _redo_queue: Array = []
+var _undo_array: Array = []
+var _redo_array: Array = []
 
 
 func run(command: BaseCommand) -> void:
 	add_child(command)
 	command.execute()
-	_undo_queue.append(command)
+	_undo_array.append(command)
 	_empty_redo()
 
 
 func undo_command() -> BaseCommand:
-	var command: BaseCommand = _undo_queue.pop_back()
+	var command: BaseCommand = _undo_array.pop_back()
 
 	if command != null:
-		_redo_queue.append(command)
+		_redo_array.append(command)
 		command.unexecute()
 
 	return command
 
 func redo_command() -> BaseCommand:
-	var command: BaseCommand = _redo_queue.pop_back()
+	var command: BaseCommand = _redo_array.pop_back()
 
 	if command != null:
-		_undo_queue.append(command)
+		_undo_array.append(command)
 		command.execute()
 
 	return command
 
 
 func _empty_undo() -> void:
-	for c in _undo_queue:
+	for c in _undo_array:
 		c.queue_free()
 	
-	_undo_queue.clear()
+	_undo_array.clear()
 
 
 func _empty_redo() -> void:
-	for c in _redo_queue:
+	for c in _redo_array:
 		c.queue_free()
 	
-	_redo_queue.clear()
+	_redo_array.clear()
 
 
 func reset_queues() -> void:
