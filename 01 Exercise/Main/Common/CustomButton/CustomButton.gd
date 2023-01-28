2021577affc8e575e53727d74d724a4109558fa8
Arpit Srivastava <skyvastern@gmail.com>
2023-01-28 16:14:05 +0530

    Update Exercise project with previous changes in Solution project

--- 01 Exercise/Main/Common/CustomButton/CustomButton.gd
+++ 01 Exercise/Main/Common/CustomButton/CustomButton.gd
@@ -1,35 +1,17 @@
-extends Control
+extends TextureButton
 class_name CustomButton
 
-var is_mouse_inside: bool = false
-const BLEND = 0.2
 
-signal press
+const BLEND = 0.2
 
 
 func _ready() -> void:
-    var _c1 = self.connect("mouse_entered", self, "_on_mouse_enter")
-    var _c2 = self.connect("mouse_exited", self, "_on_mouse_exit")
-    var _c3 = self.connect("press", self, "_on_mouse_press")
+	var _c1 = self.connect("button_up", self, "_on_button_up")
+	var _c2 = self.connect("button_down", self, "_on_button_down")
 
 
-func _input(event: InputEvent) -> void:
-    if is_mouse_inside and event is InputEventMouseButton:
-        if event.button_index == BUTTON_LEFT and event.pressed:            
-            emit_signal("press")
+func _on_button_up() -> void:
+	$AnimationPlayer.play("Idle", BLEND)
 
-
-func _on_mouse_enter() -> void:
-    is_mouse_inside = true
-    $AnimationPlayer.play("Highlight", BLEND)
-
-func _on_mouse_exit() -> void:
-    is_mouse_inside = false
-    $AnimationPlayer.play("Idle", BLEND)
-
-func _on_mouse_press() -> void:
-    $AnimationPlayer.play("Pressed", BLEND)
-    if is_mouse_inside:
-        $AnimationPlayer.play("Highlight", BLEND)
-    else:
-        $AnimationPlayer.play("Idle", BLEND)
+func _on_button_down() -> void:
+	$AnimationPlayer.play("Pressed", BLEND)
--- 01 Exercise/Main/UI/Edit.gd
+++ 01 Exercise/Main/UI/Edit.gd
@@ -8,10 +8,19 @@ onready var new_game: CustomButton = get_node(new_game_path)
 onready var undo: CustomButton = get_node(undo_path)
 onready var redo: CustomButton = get_node(redo_path)
 
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("new_game"):
-		new_game.emit_signal("press")
+		new_game.emit_signal("button_down")
+	elif Input.is_action_just_released("new_game"):
+		new_game.emit_signal("button_up")
+
 	elif Input.is_action_just_pressed("undo"):
-		undo.emit_signal("press")
+		undo.emit_signal("button_down")
+	elif Input.is_action_just_released("undo"):
+		undo.emit_signal("button_up")
+
 	elif Input.is_action_just_pressed("redo"):
-		redo.emit_signal("press")
+		redo.emit_signal("button_down")
+	elif Input.is_action_just_released("redo"):
+		redo.emit_signal("button_up")
