11278d885cbf73f954ab7e809cb15a6a2d8424cd
Arpit Srivastava <skyvastern@gmail.com>
2023-01-28 15:57:53 +0530

    Update HTML5 build

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674830174|46190579";
+const CACHE_VERSION = "1674901351|2748283210";
 const CACHE_PREFIX = "Command Pattern-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
